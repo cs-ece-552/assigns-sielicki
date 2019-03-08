aded03c826f490428a9cf0a08517387cedf70502
Chetan Mistry <cmistry98@gmail.com>
2019-03-07 19:16:30 -0600

    fixed bugs

--- hw/hw6/hw6_1/xnor5_1.v
+++ hw/hw6/hw6_1/xnor5_1.v
@@ -4,11 +4,11 @@ module xnor5_1(opcode, func, out);
 
    wire        xor0, xor1, xor2, xor3, xor4;
 
-   assign xor0 = opcode[0] ^ func[0];
-   assign xor1 = opcode[1] ^ func[1];
-   assign xor2 = opcode[2] ^ func[2];
-   assign xor3 = opcode[3] ^ func[3];
-   assign xor4 = opcode[4] ^ func[4];
+   assign xor0 = ~(opcode[0] ^ func[0]);
+   assign xor1 = ~(opcode[1] ^ func[1]);
+   assign xor2 = ~(opcode[2] ^ func[2]);
+   assign xor3 = ~(opcode[3] ^ func[3]);
+   assign xor4 = ~(opcode[4] ^ func[4]);
 
    assign out = xor0 & xor1 & xor2 & xor3 & xor4;
 
