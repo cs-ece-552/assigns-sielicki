40176fccd3ebb60917f14b0e1ff9efb74331c2d9
Nicholas Sielicki <sielicki@yandex.com>
2019-03-09 17:43:47 -0600

    Add tests

---
+++ hw/hw6/tests/sle/sle_1.asm
@@ -0,0 +1,6 @@
+// Should set rd with two equal negative operands    : (rs == rt) where ((rs,rt) < 0)
+	lbi    r1, -1
+	lbi    r2, -1
+	sle    r3, r1, r2
+	halt
+//  Expected: r3 == 1, (r1,r2) == -1
---
+++ hw/hw6/tests/sle/sle_11.asm
@@ -0,0 +1,6 @@
+// Should not set rt with opposite sign operands         : (rt < rs)  where (rs < 0, 0 < rt)
+	lbi    r1, 4
+	lbi    r2, -4
+	sle    r3, r1, r2
+	halt
+//  Expected: r3 == 0, r1 == 4, r2 == -4
