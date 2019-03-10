5680c812ba116ac4d0afaae37e1208968e2cc04a
Chetan Mistry <cmistry98@gmail.com>
2019-03-10 00:14:53 -0600

    PCAdder Logic Done

---
+++ project/demo1/verilog/d-iSig.v
@@ -0,0 +1,12 @@
+module d_iSig(/*AUTOARGS*/)
+  input[4:0] opcode;
+   output    out;
+   wire [1:0]   funct;
+
+   xnor5_1 xnor0 (.opcode(opcode), .func(5'b00100), .out(funct[0]));
+   xnor5_1 xnor1 (.opcode(opcode), .func(5'b00110), .out(funct[1]));
+
+   assign out = funct[0] | funct[1];
+endmodule; // d_iSig
+
+
