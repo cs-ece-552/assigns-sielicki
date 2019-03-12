e14fc1fce65f50db6b3d1439f9764f1a1fc40b4a
Jeremy Intan <intan@snares-01.cs.wisc.edu>
2019-03-11 21:39:31 -0500

    fixed the test bench, removed halt, needs adding

--- project/demo1/verilog/proc_hier_bench.v
+++ project/demo1/verilog/proc_hier_bench.v
@@ -124,31 +124,52 @@ module proc_hier_bench();
    // Edit the example below. You must change the signal
    // names on the right hand side
     
-   assign PC = DUT.p0.fetch0.pcCurrent;
-   assign Inst = DUT.p0.fetch0.instr;
+   assign PC = DUT.p0.instructionmem.addr;
    
-   assign RegWrite = DUT.p0.decode0.regFile0.write;
+   //assign PC = DUT.p0.fetch0.pcCurrent;
+   assign Inst = DUT.p0.instructionmem.data_out;
+   
+   //assign Inst = DUT.p0.fetch0.instr;
+   
+   assign RegWrite = DUT.p0.control.RegWrite;
+   // assign RegWrite = DUT.p0.decode0.regFile0.write;
    // Is register being written, one bit signal (1 means yes, 0 means no)
+
+
+
+   assign WriteRegister = DUT.p0.rf_bypass.writeRegSel;
    
-   assign WriteRegister = DUT.p0.decode0.regFile0.writeregsel;
+   //   assign WriteRegister = DUT.p0.decode0.regFile0.writeregsel;
    // The name of the register being written to. (3 bit signal)
 
-   assign WriteData = DUT.p0.decode0.regFile0.writedata;
+   assign WriteData = DUT.p0.rf_bypass.writeData;
+
+   //assign WriteData = DUT.p0.decode0.regFile0.writedata;
    // Data being written to the register. (16 bits)
    
-   assign MemRead =  DUT.p0.memory0.memRead;
+   assign MemRead = DUT.p0.control.MemToReg;
+   
+ //assign MemRead =  DUT.p0.memory0.memRead;
    // Is memory being read, one bit signal (1 means yes, 0 means no)
+
+   assign MemWrite = (DUT.p0.control.DMemEn & DUT.p0.control.DMemWrite);
    
-   assign MemWrite = (DUT.p0.memory0.memReadorWrite & DUT.p0.memory0.memWrite);
+   //assign MemWrite = (DUT.p0.memory0.memReadorWrite & DUT.p0.memory0.memWrite);
    // Is memory being written to (1 bit signal)
+
+   assign MemAddress = DUT.p0.alu.res;
    
-   assign MemAddress = DUT.p0.memory0.aluResult;
+   //assign MemAddress = DUT.p0.memory0.aluResult;
    // Address to access memory with (for both reads and writes to memory, 16 bits)
+
+   assign MemData = DUT.p0.datamem.data_in;
    
-   assign MemData = DUT.p0.memory0.writeData;
+   //assign MemData = DUT.p0.memory0.writeData;
    // Data to be written to memory for memory writes (16 bits)
+
+//   assign Halt = DUT.p0.
    
-   assign Halt = DUT.p0.memory0.halt;
+   //assign Halt = DUT.p0.memory0.halt;
    // Is processor halted (1 bit signal)
    
    /* Add anything else you want here */
