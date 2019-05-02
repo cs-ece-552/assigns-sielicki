ac060187d4cf52b6e1c652bf5f2e84b7f5c956b3
Jeremy Intan <intan@rockhopper-03.cs.wisc.edu>
2019-05-01 23:14:06 -0500

    finish dc_test.asm

--- project/demo3/verification/dc_test.asm
+++ project/demo3/verification/dc_test.asm
@@ -4,3 +4,22 @@ slbi r1, 0
 lbi r2, 2
 lbi r3, 3
 lbi r4, 4
+
+st r2, r0, 0
+st r3, r0, 2
+st r4, r0, 4
+st r2, r1, 4
+st r3, r1, 2
+st r4, r1, 6
+st r2, r0, 10
+st r3, r0, 12
+st r4, r0, 8
+ld r5, r0, 0
+ld r6, r0, 2
+ld r7, r0, 4
+ld r5, r1, 4
+ld r6, r1, 2
+ld r7, r1, 6
+ld r5, r0, 10
+ld r6, r0, 12
+ld r7, r0, 8
