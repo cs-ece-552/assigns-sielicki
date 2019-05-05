7053f070111e182e297af7b637f884e940f7f5b2
Jeremy Intan <intan@snares-06.cs.wisc.edu>
2019-05-05 17:20:22 -0500

    Added test and verification

---
+++ project/demo3/verification/lru.asm
@@ -0,0 +1,21 @@
+lbi r0, 0
+lbi r1, 8
+slbi r1, 0
+add r2, r1, r1
+add r3, r2, r1
+add r4, r3, r1
+add r5, r4, r1
+add r6, r5, r1
+ld r7, r1, 0
+ld r7, r0, 0
+ld r7, r2, 0
+ld r7, r0, 0
+ld r7, r3, 0
+ld r7, r0, 0
+ld r7, r4, 0
+ld r7, r0, 0
+ld r7, r5, 0
+ld r7, r0, 0
+ld r7, r6, 0
+ld r7, r0, 0
+halt
